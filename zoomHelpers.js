c582802ccf5d00eaa51a371613f5db2749f3f61d
nickfreemandesign <nicho.freeman@gmail.com>
2018-04-27 16:50:39 -0700

    working implementation of fetching live room results from 4 different accounts

--- zoomHelpers.js
+++ zoomHelpers.js
@@ -6,12 +6,13 @@ let qs = require('querystring');
 let key = 'eCWUVjT_QI21z6r7sltq8A';
 let secret = 'lmOeqSkPi1rBLnih1Z6FZy68XT2SxZkDvFMR';
 
+// acctIDs and names
 let acctMain = ['-wVNuo2JRFynXliZS92Wpg','acctMain'];
 let acctBaobob = ['4wJXp2PtRV-2pJomy4FcfQ','acctBaobob'];
 let acctCherryblossom = ['DnSMCDCJQnerUndvwyL9QA','acctCherryblossom'];
 let acctDogwood = ['pg5-t-fZRYauwh9gywSn5w','acctDogwood'];
 
-
+//set time for token validity
 let convertToSeconds = 1000; // 1000 ms in 1 second
 let secondsToAdd = 3600; // 3600 seconds in 1 hour
 let hour = secondsToAdd * convertToSeconds;
@@ -21,23 +22,27 @@ let payload = {
   iss: key,
   exp: ((new Date()).getTime() + hour)
 };
-
 let token = jwt.sign(payload, secret);
 
+//global results array that gets populated and then handed over to attendanceBuilder
 let results = [];
 
+//accts in collection
 let acctIdArr = [ acctBaobob, acctCherryblossom, acctDogwood, acctMain ];
 
+//setTimeout helper function
 function delay(ms, cb){
   setTimeout(cb,ms)
 }
 
+//1 second promise delay helper function. possibly not needed.
 function oneSecondDelay() {
   return new Promise(resolve => {
     setTimeout(()=>{resolve()}, 1000)
   })
 }
 
+//zoom api call to get live meetings held by acctId
 function getAllLiveMeetings(acctId) {
   let zoomOptions = {
     url: `https://api.zoom.us/v2/users/${acctId}/meetings`,
@@ -69,7 +74,7 @@ function getAllLiveMeetings(acctId) {
   })
   //end fcn
 }
-
+//zoom api call to get live participant data given room instance uuid
 function getCurrentRoomStats(roomUUId) {
 
   let zoomOptions = {
@@ -101,11 +106,15 @@ function getCurrentRoomStats(roomUUId) {
   })
   //end fcn
 }
-
+//global counter to help iterate across all live rooms collected
 let counter  = 0;
-
+//function blocking loop to get room stats once live room uuids have been collected
 function resultsLoop(cb){
    setTimeout(async function () {
+     if (results.length === 0 || results[counter] === undefined || results[counter].uuid === undefined) {
+       cb([])
+       return
+     }
       let attendance = await getCurrentRoomStats(results[counter].uuid)
       attendance = JSON.parse(attendance)
       results[counter]['liveAttendance'] = attendance.participants
@@ -114,27 +123,28 @@ function resultsLoop(cb){
          resultsLoop(cb);
       } else {
         cb(results)
+        return
       }
    }, 1000)
 }
 
-async function globalAttendance(acctId) {
+async function globalAttendance(acctId, uselessNum) {
+  let dataCar = "🚐 ... ";
+  let dataConvoy = Array(uselessNum).join(dataCar)
+  console.log(`${dataConvoy} Asking Zoom for datas from ${acctId[1]}`)
   let allMtgs = await getAllLiveMeetings(acctId[0])
   allMtgs = JSON.parse(allMtgs)
   allMtgs.meetings.forEach(mtg => {
     mtg['host_name'] = acctId[1]
   })
   results = results.concat(allMtgs.meetings)
-  await oneSecondDelay()
-
-
 }
 
 function getLiveAttendance(cb) {
-  delay(1000, ()=>{globalAttendance(acctIdArr[0])})
-  delay(2100, ()=>{globalAttendance(acctIdArr[1])})
-  delay(3200, ()=>{globalAttendance(acctIdArr[2])})
-  delay(4300, ()=>{globalAttendance(acctIdArr[3])})
+  delay(1000, ()=>{globalAttendance(acctIdArr[0],1)})
+  delay(2100, ()=>{globalAttendance(acctIdArr[1],2)})
+  delay(3200, ()=>{globalAttendance(acctIdArr[2],3)})
+  delay(4300, ()=>{globalAttendance(acctIdArr[3],4)})
   delay(8000, ()=>{resultsLoop(cb)})
 }
 
