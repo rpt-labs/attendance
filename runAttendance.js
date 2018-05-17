820ff85df1a2358c640c2d67f56a71de9af00217
nickfreemandesign <nicho.freeman@gmail.com>
2018-05-16 18:51:08 -0700

    working implmentation with new refactor in place

--- runAttendance.js
+++ runAttendance.js
@@ -2,10 +2,11 @@ var fs = require('fs');
 var Papa = require('papaparse');
 var Fuse = require('fuse.js');
 let zutils = require('./zoomHelpers');
+let sampleData = require('./sample-data')
 
 var cohortsToCheck = process.argv.slice(2)
 
-console.log(cohortsToCheck)
+// console.log(cohortsToCheck)
 
 var options = {
 	delimiter: ",",	// auto-detect
@@ -104,50 +105,112 @@ function flattenZoomResults(zoomResults){
 async function runAttendance(zoomResults){
 
   let allStudents = await getRptRoster();
+
   let studentsExpected = allStudents.filter( stu => {
     return cohortsToCheck.includes(stu.cohort)
-  })
+  });
+
   let studentsPresent = flattenZoomResults(zoomResults);
 
-
+	let studentsOutput = [];
   let studentsAbsent = [];
-  let options = {
-  shouldSort: true,
-  threshold: 0.7,
-  location: 0,
-  // includeScore: true,
-  // distance: 100,
-  maxPatternLength: 32,
-  minMatchCharLength: 4,
-  keys: [
-    {name: "user_name", weight: 0.9},
-    // {name: "firstName", weight: 0.9},
-    // {name: "lastName", weight: 0.7},
-    // {name: "moreName", weight: 0.8},
-  ],
-  id: "user_name"
-  };
 
-  console.log("⛱ ⛱ ⛱ ⛱ ⛱ BEGIN STUDENTS PRESENT ⛱ ⛱ ⛱ ⛱ ⛱");
-  console.log(studentsPresent);
-  console.log("⛱ ⛱ ⛱ ⛱ ⛱ END STUDENTS PRESENT ⛱ ⛱ ⛱ ⛱ ⛱");
+  // console.log("⛱ ⛱ ⛱ ⛱ ⛱ BEGIN STUDENTS PRESENT ⛱ ⛱ ⛱ ⛱ ⛱");
+  // console.log(studentsExpected);
+  // console.log("⛱ ⛱ ⛱ ⛱ ⛱ END STUDENTS PRESENT ⛱ ⛱ ⛱ ⛱ ⛱");
 
-
-
+	// loop through student roster, which was filtered into cohorts expected
   for (var i = 0; i < studentsExpected.length; i++) {
-    let student = studentsExpected[i]
-    var fuse = new Fuse(studentsPresent, options); // "list" is the item array
-    var match = fuse.search(student['full_name']);
+		// create variables
+    let student = studentsExpected[i];
+		let studentFullName = student.full_name.toLowerCase();
+		let studentNameCollection = studentFullName.split(' ');
+		let studentConcatName = studentNameCollection.join('')
+		let studentEmail = student.email.split('@')[0];
+		let studentEmailCollection = null;
+		if (studentEmail.indexOf('.') > -1) {
+			studentEmailCollection = studentEmail.split('.');
+		}
+		// console.log(student)
+		//create student output object
+		let studentOutput = {
+			name: studentFullName,
+			cohort: student.cohort,
+			match: null,
+			absent: true,
+		}
+		// loop through studentsPresent, a flat collection originating
+		// from zutils.getLiveAttendance(runAttendance)
+		for (var j = 0; j < studentsPresent.length; j++) {
+			let studentZoom = studentsPresent[j];
+			let studentZoomName = studentZoom.user_name.toLowerCase();
+			// see if zoom name can be found in students full name
+			// first try direct match
+			if (studentFullName.indexOf(studentZoomName) > -1 ) {
+					studentOutput.absent = false;
+					studentOutput.match = `full name: ${studentFullName}`
+			//try email before @
+			} else if (!studentEmail && studentEmail.indexOf(studentZoomName) > -1 ) {
+					studentOutput.absent = false;
+					studentOutput.match = `email: ${studentEmail}`
+			} else if (studentConcatName.indexOf(studentZoomName) > -1) {
+					studentOutput.absent = false;
+					studentOutput.match = `concatName: ${studentConcatName}`
+			} else {
+				//try student's name split by spaces ' '
+				studentNameCollection.forEach((el, idx) => {
+					if (el.indexOf(studentZoomName) > -1) {
+						studentOutput.absent = false;
+						studentOutput.match = `name: ${idx} idx (${el}) of ${studentNameCollection.join(' ')}`
+					}
+				})
+				if (studentEmailCollection) {
+					//try student's email split by periods '.'
+					studentEmailCollection.forEach((el, idx) => {
+						if (el.indexOf(studentZoomName) > -1) {
+							studentOutput.absent = false;
+							studentOutput.match = `email: ${idx} idx (${el}) of ${studentEmailCollection.join(' ')}`
+						}
+					})
+				}
+			}
+		}
 
-    if (match.length > 0) {
-      console.log(`${student['full_name']}:   ${JSON.stringify(match)}`);
-      console.log('')
+		studentsOutput.push(studentOutput)
+  }
 
-    } else {
-      console.log(`${student['full_name']}:   no match`);
-      console.log('')
+	let attendanceObj = {};
+
+	studentsOutput.forEach(el=>{
+		if (!attendanceObj[el.cohort]) {
+			attendanceObj[el.cohort] = [];
+		}
+
+		attendanceObj[el.cohort].push(el)
+	})
+	// console.log(attendanceObj)
+	printAttendance(attendanceObj);
+	return
+}
+
+function printAttendance(attendanceObj){
+  for (let cohort in attendanceObj) {
+    console.log(' ')
+    console.log(`--------------------- ${cohort} ---------------------`);
+    for (let i = 0; i < attendanceObj[cohort].length; i++) {
+      if ( !attendanceObj[cohort][i].absent ) {
+        console.log( `${attendanceObj[cohort][i].name} matched ${attendanceObj[cohort][i].match} √`  )
+      } else {
+        let stu = attendanceObj[cohort][i];
+        let len = 49 - ( attendanceObj[cohort][i].name.length + 4 )
+        let buffer = Array(len).join('-')
+        console.log( stu.name, `<---${buffer}  ABSENT`  )
+      }
     }
   }
 }
+
+//NOTE: available for testing
+// runAttendance(sampleData)
 
 zutils.getLiveAttendance(runAttendance)
