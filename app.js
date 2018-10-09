333383c364764e7b4e379d04804b186775af9001
michellelockett <shelldog42@gmail.com>
2018-10-08 17:53:05 -0700

    creates express route for running live attendance with no log

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ require('dotenv').config();
 var express = require('express');
 var port = process.env.SERVER_PORT;
 const { getAbsenceData } = require('./utils/analyzeAbsences');
+const zutils = require('./zoomHelpers');
+const { getAttendanceNoLog } = require('./utils/takeAttendanceNoLog');
+const asyncMiddleware = require('./utils/asyncMiddleware');
 
 var app = express();
 app.use(express.static('public'));
-app.get('/absences', async function(req, res) {
+app.get('/absences', asyncMiddleware(async(req, res) => {
   let absences = await getAbsenceData();
   res.send(absences);
-});
+}));
+
+app.get('/takeAttendance/:cohorts', asyncMiddleware(async(req, res) => {
+  let { cohorts } = req.params;
+  cohorts = cohorts.split('+');
+  let rawAttendance = await zutils.getLiveAttendanceNoLog();
+  let formattedAttendance = await getAttendanceNoLog(rawAttendance, cohorts);
+  res.send({formattedAttendance});
+}));
 
 app.listen(port, () => console.log(`listening on port ${port}`));
