f4748515f94347cbe0f683ea6c1aafa4f044788a
michellelockett <shelldog42@gmail.com>
2018-10-09 08:58:55 -0700

    updates route to use cors

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
-var express = require('express');
-var port = process.env.SERVER_PORT;
+const express = require('express');
+const port = process.env.SERVER_PORT;
+const cors = require('cors');
+const { getAttendanceNoLog } = require('./utils/takeAttendanceNoLog');
 const { getAbsenceData } = require('./utils/analyzeAbsences');
 const zutils = require('./zoomHelpers');
-const { getAttendanceNoLog } = require('./utils/takeAttendanceNoLog');
 const asyncMiddleware = require('./utils/asyncMiddleware');
 
 var app = express();
@@ -13,10 +14,9 @@ app.get('/absences', asyncMiddleware(async(req, res) => {
   res.send(absences);
 }));
 
-app.get('/takeAttendance/:cohorts', asyncMiddleware(async(req, res) => {
+app.get('/takeAttendance/:cohorts', cors(), asyncMiddleware(async(req, res) => {
   let { cohorts } = req.params;
   cohorts = cohorts.split('+');
-  let results = {};
   let rawAttendance = await zutils.getLiveAttendanceNoLog();
   let formattedAttendance = await getAttendanceNoLog(rawAttendance, cohorts);
   //let sortedStudents = formattedAttendance.present.sort((a, b) => a.timeJoined > b.timeJoined);
